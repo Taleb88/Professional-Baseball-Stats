efa58a43db0ba8eab39d70e5555ab1d35e573c35
Taleb88 <tdamaree18@gmail.com>
2025-02-16 18:56:32 -0500

    conducting tests

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import pandas as pd
 # 2/16/2025 - IN PROGRESS
 for x in range(2016,2019):
     los_angeles_dodgers_offensive_stats_df = pd.read_csv(f'mlb/{x}_los_angeles_dodgers_offensive_stats.csv')
+    print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
     
-    def filtering(df):
+    def player_column(df):
         try: 
             return df[(df['Player'] != 'Player') & 
                     (df['Player'] != 'Team Totals') & 
@@ -14,8 +15,10 @@ for x in range(2016,2019):
         except Exception as e:
             print('cannot filter rows of dataframe accordingly')
 
-    los_angeles_dodgers_offensive_stats_df = filtering(los_angeles_dodgers_offensive_stats_df)
+    los_angeles_dodgers_offensive_stats_df = player_column(los_angeles_dodgers_offensive_stats_df)
     los_angeles_dodgers_offensive_stats_df['RBI'] = los_angeles_dodgers_offensive_stats_df['RBI'].astype(int)
+    los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by=['RBI'], ascending=False)
+    los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.drop('Rk', axis=1)
     los_angeles_dodgers_offensive_stats_df.to_csv(f'{x}_los_angeles_dodgers_offensive_stats.csv', index=False)
 
     print(f'\n{x}_los_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
