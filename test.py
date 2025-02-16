a2dd93b70d0a9fd5b3b4a517bff465e281f57419
Taleb88 <tdamaree18@gmail.com>
2025-02-16 18:16:14 -0500

    conducting tests

--- test.py
+++ test.py
@@ -2,27 +2,31 @@ import pandas as pd
 
 
 # 2/16/2025 - IN PROGRESS
-los_angeles_dodgers_offensive_stats_df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
+# los_angeles_dodgers_offensive_stats_df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
 
-print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
+# print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 
-def filtering(df):
-    try: 
-        return df[(df['Player'] != 'Player') & 
-                (df['Player'] != 'Team Totals') & 
-                (df['Player'] != 'Non-Pitcher Totals') & 
-                (df['Player'] != 'Pitcher Totals')]
-    except Exception as e:
-        print('cannot filter rows of dataframe accordingly')
+# def filtering(df):
+#     try: 
+#         return df[(df['Player'] != 'Player') & 
+#                 (df['Player'] != 'Team Totals') & 
+#                 (df['Player'] != 'Non-Pitcher Totals') & 
+#                 (df['Player'] != 'Pitcher Totals')]
+#     except Exception as e:
+#         print('cannot filter rows of dataframe accordingly')
 
-los_angeles_dodgers_offensive_stats_df = filtering(los_angeles_dodgers_offensive_stats_df)
+# los_angeles_dodgers_offensive_stats_df = filtering(los_angeles_dodgers_offensive_stats_df)
 
-los_angeles_dodgers_offensive_stats_df['RBI'] = los_angeles_dodgers_offensive_stats_df['RBI'].astype(int)
-los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
+# los_angeles_dodgers_offensive_stats_df['RBI'] = los_angeles_dodgers_offensive_stats_df['RBI'].astype(int)
+# los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
 
-print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
+# print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 
-los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by=['RBI'],ascending=False)
+# los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by=['RBI'],ascending=False)
 
-print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
-los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
+# print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
+# los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
+
+for x in range(2016,2019):
+    los_angeles_dodgers_offensive_stats_df = pd.read_csv(f'mlb/{x}_los_angeles_dodgers_offensive_stats.csv')
+    print(f'\n{x}_los_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
