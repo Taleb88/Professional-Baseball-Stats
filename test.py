9cc05d86f84a94b28e2a9131c85c94925ef98d14
Taleb88 <tdamaree18@gmail.com>
2025-02-16 18:08:24 -0500

    conducting tests

--- test.py
+++ test.py
@@ -2,26 +2,26 @@ import pandas as pd
 
 
 # 2/16/2025 - IN PROGRESS
-df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
+los_angeles_dodgers_offensive_stats_df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
 
 # df = df.loc[df['Player'] != 'Player']
-df = df.sort_values(by=['RBI'], ascending=False)
+los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by='RBI', ascending=False)
 
-df.to_csv('test.csv', index=False)
+print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 
 def filtering(df):
     try: 
-        return df[(df['Player'] != 'Player') | 
-                (df['Player'] != 'Team Totals') | 
-                (df['Player'] != 'Non-Pitcher Totals') | 
+        return df[(df['Player'] != 'Player') & 
+                (df['Player'] != 'Team Totals') & 
+                (df['Player'] != 'Non-Pitcher Totals') & 
                 (df['Player'] != 'Pitcher Totals')]
     except Exception as e:
         print('cannot filter rows of dataframe accordingly')
 
-df = filtering(df)
+los_angeles_dodgers_offensive_stats_df = filtering(los_angeles_dodgers_offensive_stats_df)
 
-df = df.sort_values(by=['RBI'], ascending=False)
+los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by=['RBI'], ascending=False)
 
-print(df)
+print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 
-df.to_csv('test.csv', index=False)
+los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
