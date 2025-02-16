092e8ef4aa3efdb990bbd33a4fbf3b77d31994d7
Taleb88 <tdamaree18@gmail.com>
2025-02-16 17:53:11 -0500

    conducting tests

--- test.py
+++ test.py
@@ -3,4 +3,20 @@ import pandas as pd
 
 # 2/16/2025 - IN PROGRESS
 df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
-print(df.sort_values(by=['RBI'], ascending=False))
+
+# df = df.loc[df['Player'] != 'Player']
+df = df.sort_values(by=['RBI'], ascending=False)
+
+df.to_csv('test.csv', index=False)
+
+def filtering(x):
+    return df[(df['Player'] != 'Player') | 
+              (df['Player'] != '') | 
+              (df['Player'] != 'Player') | 
+              (df['Player'] != 'Player').sort_values(by=['RBI'], ascending=False)]
+
+df = filtering(df)
+
+print(df)
+
+df.to_csv('test.csv', index=False)
