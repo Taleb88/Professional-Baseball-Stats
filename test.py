ceec0d250a8c90b3004198de355a43b9319748fc
Taleb88 <tdamaree18@gmail.com>
2025-02-16 18:17:26 -0500

    conducting tests

--- test.py
+++ test.py
@@ -2,6 +2,22 @@ import pandas as pd
 
 
 # 2/16/2025 - IN PROGRESS
+for x in range(2016,2019):
+    los_angeles_dodgers_offensive_stats_df = pd.read_csv(f'mlb/{x}_los_angeles_dodgers_offensive_stats.csv')
+    print(f'\n{x}_los_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
+    
+    def filtering(df):
+        try: 
+            return df[(df['Player'] != 'Player') & 
+                    (df['Player'] != 'Team Totals') & 
+                    (df['Player'] != 'Non-Pitcher Totals') & 
+                    (df['Player'] != 'Pitcher Totals')]
+        except Exception as e:
+            print('cannot filter rows of dataframe accordingly')
+
+    los_angeles_dodgers_offensive_stats_df = filtering(los_angeles_dodgers_offensive_stats_df)    
+
+# 2/16/2025 - IN PROGRESS - SUCCESS
 # los_angeles_dodgers_offensive_stats_df = pd.read_csv('mlb/2016_los_angeles_dodgers_offensive_stats.csv')
 
 # print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
@@ -26,7 +42,3 @@ import pandas as pd
 
 # print('\nlos_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 # los_angeles_dodgers_offensive_stats_df.to_csv('2016_los_angeles_dodgers_offensive_stats.csv', index=False)
-
-for x in range(2016,2019):
-    los_angeles_dodgers_offensive_stats_df = pd.read_csv(f'mlb/{x}_los_angeles_dodgers_offensive_stats.csv')
-    print(f'\n{x}_los_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
