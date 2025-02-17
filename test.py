e14319865a453acabb9fa67c3a21edb3adc372dd
Taleb88 <tdamaree18@gmail.com>
2025-02-16 19:00:44 -0500

    conducting tests

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ for x in range(2016,2019):
     los_angeles_dodgers_offensive_stats_df['RBI'] = los_angeles_dodgers_offensive_stats_df['RBI'].astype(int)
     los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.sort_values(by=['RBI'], ascending=False)
     los_angeles_dodgers_offensive_stats_df = los_angeles_dodgers_offensive_stats_df.drop('Rk', axis=1)
-    los_angeles_dodgers_offensive_stats_df.to_csv(f'{x}_los_angeles_dodgers_offensive_stats.csv', index=False)
+    los_angeles_dodgers_offensive_stats_df.to_csv(f'csv/{x}_los_angeles_dodgers_offensive_stats.csv', index=False)
 
     print(f'\n{x}_los_angeles_dodgers_offensive_stats_df:\n',los_angeles_dodgers_offensive_stats_df)
 
