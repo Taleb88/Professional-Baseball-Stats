d1039203dcb8131e2120cd6e4c6498a15e0accd7
Taleb88 <tdamaree18@gmail.com>
2025-03-29 19:01:55 -0400

    added 2 files pertaining to negro leagues; data to be included in those files - in progress

---
+++ negro_leagues.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import time
+
+# kansas city monarchs - 1924-1931 and 1937-1948 seasons only available via baseball-reference.com
+for year in range(1924,1932):
+    try: 
+        kansas_city_monarchs_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/KCM/{year}.shtml')
+        kansas_city_monarchs_offensive_stats_df = kansas_city_monarchs_stats_df[0]
+        kansas_city_monarchs_offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv')
+        time.sleep(20)
+        for year in range(1937,1949):
+            try: 
+                kansas_city_monarchs_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/KCM/{year}.shtml')
+                kansas_city_monarchs_offensive_stats_df = kansas_city_monarchs_stats_df[0]
+                kansas_city_monarchs_offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv')
+                time.sleep(20)
+            except Exception as e:
+                print(f'cannot filter rows for 1937-49 offensive stats accordingly - {type(e)}')        
+    except Exception as e:
+        print(f'cannot filter rows for 1924-1931 offensive stats accordingly - {type(e)}')
