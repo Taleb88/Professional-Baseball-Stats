09ef43b6fe17d7a76160f056b021e5dfe7bd7e9f
Taleb88 <tdamaree18@gmail.com>
2025-03-29 23:18:00 -0400

    code refactored in negro_leagues.py

--- negro_leagues.py
+++ negro_leagues.py
@@ -6,7 +6,7 @@ for year in range(1924,1949):
     try: 
         kansas_city_monarchs_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/KCM/{year}.shtml')
         kansas_city_monarchs_offensive_stats_df = kansas_city_monarchs_stats_df[0]
-        kansas_city_monarchs_offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv')
+        kansas_city_monarchs_offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv',index=False)
         time.sleep(20)    
     except Exception as e:
         print(f'offensive stats data does not exist for the {year} Season - {type(e)}')    
