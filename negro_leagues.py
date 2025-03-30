eea7ece112161c446405cf848e23624f62b3b9ac
Taleb88 <tdamaree18@gmail.com>
2025-03-29 23:35:38 -0400

    code refactored in negro_leagues.py

--- negro_leagues.py
+++ negro_leagues.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import time
 
-# kansas city monarchs - 1924-1931 and 1937-1948 seasons only available via baseball-reference.com
+franchise = ['KCM','CAG','BBB','MRS','DS','SLS','CSW','ABC','CBN']
 for year in range(1924,1949):
     try: 
-        kansas_city_monarchs_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/KCM/{year}.shtml')
-        kansas_city_monarchs_offensive_stats_df = kansas_city_monarchs_stats_df[0]
-        kansas_city_monarchs_offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv',index=False)
-        time.sleep(20)    
+        for team_name in franchise:
+            try:
+                overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/{team_name}/{year}.shtml')
+                offensive_stats_df = overall_stats_df[0]
+                time.sleep(20)  
+                if 'KCM' in franchise:
+                    offensive_stats_df.to_csv(f'negro_leagues/{year}_kansas_city_monarchs_offensive_stats.csv',index=False)
+                if 'CAG' in franchise:
+                    offensive_stats_df.to_csv(f'negro_leagues/{year}_chicago_american_giants_offensive_stats.csv',index=False)
+            except Exception as e:
+                print(f'offensive stats data for {team_name} does not exist for the {year} Season - {type(e)}')       
     except Exception as e:
-        print(f'offensive stats data does not exist for the {year} Season - {type(e)}')    
+        print(f'cannot filter rows accordingly - {type(e)}')    
