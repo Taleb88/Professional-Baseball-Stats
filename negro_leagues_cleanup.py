3a5a854428ddac2e826499edfffc7c6c418dba75
Taleb88 <tdamaree18@gmail.com>
2025-04-05 17:10:11 -0400

    negro_leagues_cleanup.py - cleanup continues

--- negro_leagues_cleanup.py
+++ negro_leagues_cleanup.py
@@ -9,19 +9,20 @@ for team in teams:
             try:
                 offensive_stats_df = pd.read_csv(f'negro_leagues/{year}_{team}_offensive_stats.csv')
                 # print(f'\n{year}_{team}_offensive_stats_df:\n',offensive_stats_df)
-                # filtering out irrelavent values in Player column from all dataframes
-                # def hitter_column(df):
-                #     try: 
-                #         return df[(df['Player'] != 'Player') & 
-                #                 (df['Player'] != 'Team Totals') & 
-                #                 (df['Player'] != 'Non-Pitcher Totals') & 
-                #                 (df['Player'] != 'Pitcher Totals')]
-                #     except Exception as e:
-                #         print(f'cannot filter rows of dataframe accordingly: {type(e)}')        
-                # offensive_stats_df = hitter_column(offensive_stats_df)
-                # offensive_stats_df[['RBI','PA','HR','SB']] = offensive_stats_df[['RBI','PA','HR','SB']].astype(int)
+                # filtering out irrelevant values in Player column from all dataframes
+                def hitter_column(df):
+                    try: 
+                        return df[(df['Player'] != 'Player') & 
+                                (df['Player'] != 'Team Totals') & 
+                                (df['Player'] != 'Non-Pitcher Totals') & 
+                                (df['Player'] != 'Pitcher Totals')]
+                    except Exception as e:
+                        print(f'cannot filter rows of dataframe accordingly: {type(e)}')        
+                offensive_stats_df = hitter_column(offensive_stats_df)
+                offensive_stats_df[['RBI','PA','HR','SB']] = offensive_stats_df[['RBI','PA','HR','SB']].astype(int)
                 # offensive_stats_df = offensive_stats_df.sort_values(by=['RBI'], ascending=False).drop('Rk', axis=1).fillna(0)
                 # offensive_stats_df[['BA','OBP','SLG','OPS','rOBA']] = offensive_stats_df[['BA','OBP','SLG','OPS','rOBA']].astype(float).replace(0, 0.000)
+                print(f'\n{year}_{team}_offensive_stats_df:\n',offensive_stats_df)
                 offensive_stats_df.to_csv(f'negro_leagues_cleanup/{year}_{team}_offensive_stats.csv',index=False)
             except Exception as e:
                 print(f'cannot certain clean csv(s) due to data not available for certain years/seasons - {type(e)}')
