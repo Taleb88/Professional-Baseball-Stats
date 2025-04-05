cb7fe0eae2f9da09697ee339b288bb53ad6f55cc
Taleb88 <tdamaree18@gmail.com>
2025-04-05 16:40:55 -0400

    negro league stats - updating cleanup

--- negro_leagues_cleanup.py
+++ negro_leagues_cleanup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-teams = ['KCM','CAG','BBB','MRS','DS'] # abbrevations per team to be filled in
+teams = ['KCM','CAG','BBB','MRS','DS','SLS','CSW','ABC','CBN']
 
 for team in teams:
     for year in range(1924,1948):
