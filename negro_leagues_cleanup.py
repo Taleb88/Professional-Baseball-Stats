43e794130244b9509ee82c1cce94f2ecdd54bbf5
Taleb88 <tdamaree18@gmail.com>
2025-04-05 16:55:58 -0400

    negro league stats - updating cleanup

--- negro_leagues_cleanup.py
+++ negro_leagues_cleanup.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-teams = ['KCM','CAG','BBB','MRS','DS','SLS','CSW','ABC','CBN']
+teams = ['kansas_city_monarchs','chicago_american_giants','birmingham_black_barons','memphis_red_sox','detroit_stars','st_louis_stars','cuban_stars_west','indianapolis_abcs','cleveland_browns']
 
 for team in teams:
-    for year in range(1924,1948):
-        try:
-            offensive_stats = pd.read_csv(f'https://www.baseball-reference.com/teams/{team}/{year}.csv')
-        except Exception as e:
-            print(f'cannot clean csv(s) - {type(e)}')
+    try:
+        for year in range(1924,1949):
+            try:
+                offensive_stats = pd.read_csv(f'negro_leagues/{year}_{team}_offensive_stats.csv')
+                offensive_stats.to_csv(f'negro_leagues_cleanup/{year}_{team}_offensive_stats.csv')
+            except Exception as e:
+                print(f'cannot clean csv(s) - {type(e)}')
+    except Exception as e:
+        print(f'unable to find team(s) - {type(e)}')    
