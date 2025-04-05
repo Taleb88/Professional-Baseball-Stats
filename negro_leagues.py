ed44eb7be7f4eecb1bc413309471676cce0e353d
Taleb88 <tdamaree18@gmail.com>
2025-04-05 16:05:56 -0400

    negro league stats - added more teams to negro_leagues.py

--- negro_leagues.py
+++ negro_leagues.py
@@ -22,6 +22,22 @@ for year in range(1924,1949):
         detroit_stars_overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/DS/{year}.shtml')
         detroit_stars_offensive_stats_df = detroit_stars_overall_stats_df[0]
         detroit_stars_offensive_stats_df.to_csv(f'negro_leagues/{year}_detroit_stars_offensive_stats.csv',index=False)
-        time.sleep(20)                   
+        time.sleep(20)
+        st_louis_stars_overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/SLS/{year}.shtml')
+        st_louis_stars_offensive_stats_df = st_louis_stars_overall_stats_df[0]
+        st_louis_stars_offensive_stats_df.to_csv(f'negro_leagues/{year}_st_louis_stars_offensive_stats.csv',index=False)
+        time.sleep(20)
+        cuban_stars_west_overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/CSW/{year}.shtml')
+        cuban_stars_west_offensive_stats_df = cuban_stars_west_overall_stats_df[0]
+        cuban_stars_west_offensive_stats_df.to_csv(f'negro_leagues/{year}_cuban_stars_west_offensive_stats.csv',index=False)
+        time.sleep(20)
+        indianapolis_abcs_overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/ABC/{year}.shtml')
+        indianapolis_abcs_offensive_stats_df = indianapolis_abcs_overall_stats_df[0]
+        indianapolis_abcs_offensive_stats_df.to_csv(f'negro_leagues/{year}_indianapolis_abcs_offensive_stats.csv',index=False)
+        time.sleep(20)
+        cleveland_browns_overall_stats_df = pd.read_html(f'https://www.baseball-reference.com/teams/CBN/{year}.shtml')
+        cleveland_browns_offensive_stats_df = cleveland_browns_overall_stats_df[0]
+        cleveland_browns_offensive_stats_df.to_csv(f'negro_leagues/{year}_cleveland_browns_offensive_stats.csv',index=False)
+        time.sleep(20)                                                             
     except Exception as e:
         print(f'data for certain seasons is not available at this time for certain teams - {type(e)}')    
