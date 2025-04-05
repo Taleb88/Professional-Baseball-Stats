31952160ed8fd3beff7599e17b5e54dd0f737e64
Taleb88 <tdamaree18@gmail.com>
2025-04-05 16:45:50 -0400

    negro league stats - updating cleanup

--- negro_leagues_cleanup.py
+++ negro_leagues_cleanup.py
@@ -5,4 +5,7 @@ teams = ['KCM','CAG','BBB','MRS','DS','SLS','CSW','ABC','CBN']
 
 for team in teams:
     for year in range(1924,1948):
-        offensive_stats = pd.read_csv(f'https://www.baseball-reference.com/teams/{team}/{year}.shtml')
+        try:
+            offensive_stats = pd.read_csv(f'https://www.baseball-reference.com/teams/{team}/{year}.csv')
+        except Exception as e:
+            print(f'cannot clean csv(s) - {type(e)}')
